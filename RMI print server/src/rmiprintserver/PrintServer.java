c5ebb61a4350f089a0a1935a6feace5d9dd1f050
nikolapeevski <Jeratull@hotmail.com>
2018-11-11 21:29:06 +0100

    (type:refactor) Add SSL related configs

--- RMI print server/src/rmiprintserver/PrintServer.java
+++ RMI print server/src/rmiprintserver/PrintServer.java
@@ -1,5 +1,7 @@
 package rmiprintserver;
 
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+import javax.rmi.ssl.SslRMIServerSocketFactory;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -7,7 +9,40 @@ import java.rmi.registry.Registry;
 public class PrintServer {
 
     public static void main(String args[]) throws RemoteException {
-        Registry registry = LocateRegistry.createRegistry(5099);
-        registry.rebind("printserver", new PrintServant());
+        setSettings();
+
+        try {
+
+            setSettings();
+
+            Registry registry = LocateRegistry.createRegistry(5099, new SslRMIClientSocketFactory(), new SslRMIServerSocketFactory(null, null, true));
+            System.out.println("RMI registry running on port " + 5099);
+
+            registry.bind("printserver", new PrintServant());
+
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    private static void setSettings() {
+
+        String pass = "password";
+
+        System.setProperty("javax.net.ssl.debug", "all");
+
+        System.setProperty("javax.net.ssl.keyStore", "C:\\ssl\\serverkeystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", pass);
+        System.setProperty("javax.net.ssl.trustStore", "C:\\ssl\\servertruststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", pass);
+    }
+
+    /**
+     * Kinda dodgy but works
+     * @return
+     */
+    private static String getProjectFolder() {
+        return System.getProperty("user.dir");
     }
 }
