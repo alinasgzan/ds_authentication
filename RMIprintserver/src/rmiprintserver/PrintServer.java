ab38eedf656853a33020988e52f668a15e8f5755
Arturs Gumenuks <arturgumenuk@gmail.com>
2018-11-25 21:05:53 +0100

    Test commit 1

--- RMIprintserver/src/rmiprintserver/PrintServer.java
+++ RMIprintserver/src/rmiprintserver/PrintServer.java
@@ -10,7 +10,7 @@ public class PrintServer {
 
     public static void main(String args[]) throws RemoteException {
         setSettings();
-
+        // testing
         try {
 
             setSettings();
